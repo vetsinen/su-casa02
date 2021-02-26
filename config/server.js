c3be52dbdcb01dd1279da8b45bac21efa94a2cb1
vetsinen <vetsinen@gmail.com>
2021-02-26 11:14:48 +0200

    admin is in

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '714ea8fee0ea4bea4db23803ece28799'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '126ab136-77d5-4229-9787-7deb8bc29700'
+};
